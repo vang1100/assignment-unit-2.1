f50ba0e28de2243fd824b181f4d44c26000abd82
Padee Vang <vangpadee11@gmail.com>
2024-06-09 11:05:36 -0500

    added first and last name varibles

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -1,7 +1,9 @@
 // REQUIRED FEATURES:
 // 1 - Create a variable called `firstName` and assign it the value of your first name
-let firstName = '';
+let firstName = 'Padee';
 // 2 - Create a second variable called `lastName` and assign it the value of your last name
+
+let lastName = 'Vang';
 
 // 3 - Create a third variable called `fullName`, assign it the value of your first and last name
 //     (remember, you already have variables for this, can you use those?)
